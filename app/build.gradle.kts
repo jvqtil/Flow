1080e320c7434db2f9ae7f214ac0c02b3ab691d1
Egor <109822937+jvqtil@users.noreply.github.com>
2026-08-27 15:41:17 +0300

    Rollback versions as update broke some functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
