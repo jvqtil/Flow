23c44e20f37d70364b4e259beea81563de9879bf
Egor <109822937+jvqtil@users.noreply.github.com>
2026-08-30 16:02:48 +0300

    Add update notification. Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "dev.jvqtil.flow"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
+        versionCode = 103
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
